053896891b5dd50037649c49c34f7ea1e8fa084c
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2023-05-23 16:47:39 -0400

    first commit

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/logo.png";
+import Link from "./link";
+import { SelectedPage } from "@/shared/types";
+
+type Props = {
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+  const flexBetween = "flex items-center justify-between";
+
+  return (
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          {/* LEFT - SIDE  */}
+          <div className={`${flexBetween} w-full gap-16`}>
+            <img alt-="logo" src={Logo} />
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Contact US"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+              </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign in</p>
+                  <button>Become a member</button>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+// <div className={`${flexBetween} mx-auto w-5/6`}>
+//   <div className={`${flexBetween} w-full gap-16`}>
+//     {/* LEFT SIDE NAV */}
+//     <img alt="logo" src={Logo} />
+//   </div>
+
+//   {/* RIGHT SIDE NAV */}
+//   <div className={`${flexBetween} w-full`}>
+//     <div className={`${flexBetween} gap-8 text-sm`}>
+    //   <Link
+    //     page="Home"
+    //     selectedPage={selectedPage}
+    //     setSelectedPage={setSelectedPage}
+    //   />
+    //   <Link
+    //     page="Benefits"
+    //     selectedPage={selectedPage}
+    //     setSelectedPage={setSelectedPage}
+    //   />
+    //   <Link
+    //     page="Our Classes"
+    //     selectedPage={selectedPage}
+    //     setSelectedPage={setSelectedPage}
+    //   />
+    //   <Link
+    //     page="Contact US"
+    //     selectedPage={selectedPage}
+    //     setSelectedPage={setSelectedPage}
+    //   />
+//     </div>
+    // <div className={`${flexBetween}`}>
+    //     <p>Sign in</p>
+    //     <button>Become a member</button>
+    // </div>
+//   </div>
+// </div>;
